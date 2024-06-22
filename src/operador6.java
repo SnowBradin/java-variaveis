933a54e4809e0874b2082ef4cd6a26ba0bffc805
Bradin <danbleeding123@gmail.com>
2024-06-22 18:15:45 -0300

    operadores-boolean2

---
+++ src/operador6.java
@@ -0,0 +1,28 @@
+public class operador6 {
+    public static void main(String[] args) {
+        String nomeUm = "Dan";
+        String nomeDois = new String("Dan");
+
+        System.out.println(nomeUm.equals(nomeDois)); //==
+
+
+        int numero1 = 1;
+        int numero2 = 2;
+
+        boolean simNao = numero1 == numero2;
+
+        if(numero1 < numero2) { //true
+            System.out.println("a nossa condicao é verdadeira");
+        }
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 != numero2;
+
+        System.out.println("numeroUm é igual a numeroDois? " + simNao);
+
+        simNao = numero1 > numero2;
+
+        System.out.println("numeroUm é maior que numeroDois? " + simNao);
+    }
+}
