50741ef215f416f45a6e4ab31de6d343372dcd6b
Bradin <danbleeding123@gmail.com>
2024-06-22 18:06:09 -0300

    operador-ternario

---
+++ src/operadorternario.java
@@ -0,0 +1,18 @@
+public class operadorternario {
+    public static void main(String[] args) {
+        int a, b;
+
+        a = 6;
+        b = 6;
+
+        String resultado = a==b ? "verdadeiro" : "falso";
+            // String resultado = "";
+            //if(a==b)
+            //  resultado = "verdadeiro";
+            //else
+            //  resultado = "falso";
+            // it's boolean (true or false)
+        System.out.println(resultado);
+}
+   
+}
