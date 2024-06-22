ed2bb15490b103f87597e86a483068ebeeadd143
Bradin <danbleeding123@gmail.com>
2024-06-22 17:59:12 -0300

    soma-e-boolean

---
+++ src/operadores4.java
@@ -0,0 +1,18 @@
+ public class operadores4 {
+    public static void main(String[] args) {
+        int numero = 5;
+
+        //x repeticao
+        numero++;
+        //numero = numero +1;
+
+        System.out.println(numero --);
+
+        System.out.println(numero);
+
+        boolean variavel = true;
+
+        System.out.println(!variavel);
+        //! inverte o valor da vareavel boolean, true or false
+    }
+ }
